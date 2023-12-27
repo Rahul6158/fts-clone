345e962efe2fb576b7067851ae6733aa07e66190
Rahul6158 <128017087+Rahul6158@users.noreply.github.com>
2023-12-27 20:32:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,23 +2,33 @@ import streamlit as st
 import page1
 import page2
 import page3
+import datetime
+import pandas as pd
+
+# Read the CSV file containing the quotes
+quotes_df = pd.read_csv('AnimeQuotes.csv')  # Replace 'path_to_your_csv_file.csv' with your file path
+
+# Function to get the quote for the day based on the date
+def get_daily_quote():
+    today = datetime.datetime.now()
+    quote_index = today.day % len(quotes_df)
+    return quotes_df.iloc[quote_index]['Quote']  # Assuming 'Quote' is the column containing the quotes
+
+# Function to display the quote at the top of the page
+def display_quote():
+    st.title("Daily Quote:")
+    quote = get_daily_quote()
+    st.write(quote)
 
 def custom_sidebar():
     st.sidebar.title("Features")
-    st.sidebar.header("Available Options")  # Add a sidebar title
-    # Create radio button group
-    page_choice = st.sidebar.radio("", ["Document and Pdf Translation", "Text Translation", "Text Summarization"])
-
-    names = ["Sai Annapurna", "Kalyan Ram Chegondi", "Vinay Bhaskar Bonam", "Karthik Vasa", "Tusha Rahul Bellamkonda", "Pindi Sushmitha Devi"]
-    st.sidebar.title("Developed By :")
-    for name in names:
-        st.sidebar.write(name)
-    st.sidebar.title("Under The Guidance of :")
-    st.sidebar.write("Dr. Bomma Ramakrishna")
-    return page_choice
+    # ... (your existing sidebar code)
 
 # Use the custom sidebar method
 page_choice = custom_sidebar()
+
+# Display the daily quote
+display_quote()
 
 if page_choice == "Document and Pdf Translation":
     page1.main()  # Call the main function for Page 1
