dfb224c9abdd2c40cc141319a08378d22cc11484
Rahul6158 <128017087+Rahul6158@users.noreply.github.com>
2023-12-27 21:45:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,16 +8,17 @@ import pandas as pd
 # Read the CSV file containing the quotes and authors
 quotes_df = pd.read_csv('AnimeQuotes.csv')  # Replace 'AnimeQuotes.csv' with your file path
 
-# Function to get the quote and author for the day based on the date
-def get_daily_quote():
-    today = datetime.datetime.now()
-    quote_index = today.day % len(quotes_df)
-    daily_quote = quotes_df.iloc[quote_index]
-    return daily_quote['Quote'], daily_quote['Character']  # Assuming 'Quote' and 'Character' are column names
+# Function to get the quote and author based on the current hour
+def get_hourly_quote():
+    current_hour = datetime.datetime.now().hour
+    quote_index = current_hour % len(quotes_df)
+    hourly_quote = quotes_df.iloc[quote_index]
+    return hourly_quote['Quote'], hourly_quote['Character']  # Assuming 'Quote' and 'Character' are column names
 
-# Function to display the daily quote and author
+# Function to display the hourly quote and author
 def display_quote():
-    quote, author = get_daily_quote()
+    st.sidebar.title("Daily Quote:")
+    quote, author = get_hourly_quote()
     
     quote_html = f'<p style="color:red; font-family: Lucida Console, Monaco, monospace; font-size: 20px;"><strong>{quote}</strong></p>'
     author_html = f'<p style="color:blue; font-family: Lucida Console, Monaco, monospace; font-size: 16px;"><em>- {author}</em></p>'
@@ -37,15 +38,24 @@ def custom_sidebar():
         st.sidebar.write(name)
     st.sidebar.title("Under The Guidance of :")
     st.sidebar.write("Dr. Bomma Ramakrishna")
-    st.header("Daily Quote:")
     
-    # Display the daily quote and author
+    # Display the hourly quote and author
     display_quote()
     
     return page_choice
 
+# Initialize session state to store the last updated hour
+if 'last_updated_hour' not in st.session_state:
+    st.session_state.last_updated_hour = -1
+
 # Use the custom sidebar method
 page_choice = custom_sidebar()
+
+# Check if an hour has passed to update the quote
+current_hour = datetime.datetime.now().hour
+if current_hour != st.session_state.last_updated_hour:
+    st.session_state.last_updated_hour = current_hour
+    st.experimental_rerun()  # Rerun the app to update the displayed quote
 
 # Depending on the selected choice, call the respective main function
 if page_choice == "Document and Pdf Translation":
