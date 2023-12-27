2d95b05c20419ea128c6f059004e40992bb08d78
Rahul6158 <128017087+Rahul6158@users.noreply.github.com>
2023-12-27 20:38:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,29 +5,40 @@ import page3
 import datetime
 import pandas as pd
 
-# Read the CSV file containing the quotes
+# Read the CSV file containing the quotes and authors
 quotes_df = pd.read_csv('AnimeQuotes.csv')  # Replace 'path_to_your_csv_file.csv' with your file path
 
-# Function to get the quote for the day based on the date
+# Function to get the quote and author for the day based on the date
 def get_daily_quote():
     today = datetime.datetime.now()
     quote_index = today.day % len(quotes_df)
-    return quotes_df.iloc[quote_index]['Quote']  # Assuming 'Quote' is the column containing the quotes
+    daily_quote = quotes_df.iloc[quote_index]
+    return daily_quote['Quote'], daily_quote['Character']  # Assuming 'Quote' and 'Author' are column names
 
-# Function to display the quote at the top of the page
+# Function to display the daily quote and author
 def display_quote():
     st.title("Daily Quote:")
-    quote = get_daily_quote()
-    st.write(quote)
+    quote, author = get_daily_quote()
+    st.write(f"\"{quote}\"")
+    st.write(f"- {author}")
 
 def custom_sidebar():
     st.sidebar.title("Features")
-    # ... (your existing sidebar code)
+    st.sidebar.header("Available Options")  # Add a sidebar title
+    # Create radio button group
+    page_choice = st.sidebar.radio("", ["Document and Pdf Translation", "Text Translation", "Text Summarization"])
 
+    names = ["Sai Annapurna", "Kalyan Ram Chegondi", "Vinay Bhaskar Bonam", "Karthik Vasa", "Tusha Rahul Bellamkonda", "Pindi Sushmitha Devi"]
+    st.sidebar.title("Developed By :")
+    for name in names:
+        st.sidebar.write(name)
+    st.sidebar.title("Under The Guidance of :")
+    st.sidebar.write("Dr. Bomma Ramakrishna")
+    return page_choice
 # Use the custom sidebar method
 page_choice = custom_sidebar()
 
-# Display the daily quote
+# Display the daily quote and author
 display_quote()
 
 if page_choice == "Document and Pdf Translation":
