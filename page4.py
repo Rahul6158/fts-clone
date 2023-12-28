951c30a36bf28abecf2642940a9a59f64a9fb061
Rahul6158 <128017087+Rahul6158@users.noreply.github.com>
2023-12-28 23:18:39 +0530

    Update page4.py

--- page4.py
+++ page4.py
@@ -1,39 +1,75 @@
 import streamlit as st
-import pandas as pd
-import base64
 
-def null_removal_page():
-    st.title("Dataset Handler - Null Value Removal")
-    uploaded_file = st.file_uploader("Upload a CSV file", type=["csv"])
+# Split the page into two columns
+left_column, right_column = st.columns(2)
 
-    if uploaded_file is not None:
-        uploaded_data = pd.read_csv(uploaded_file)
+# Add content to the left column
+with left_column:
+    st.header("Left Side")
+    # Add your content for the left side here
 
-        # Display basic details about the dataset
-        st.subheader("Dataset Information:")
-        st.write(f"Number of Rows: {uploaded_data.shape[0]}")
-        st.write(f"Number of Columns: {uploaded_data.shape[1]}")
-        st.write("Preview of the Dataset:")
-        st.write(uploaded_data.head())
+def display_method_info():
+    st.sidebar.title("Method Functionality")
 
-        # Show null value counts
-        st.subheader("Null Values in the Dataset:")
-        st.write(uploaded_data.isnull().sum())
+    method_info = {
+        "process_docx_text": {
+            "Functionality": "Extracts text from a DOCX file.",
+            "Parameters": "docx_file (DOCX file path)",
+            "Output": "Extracted text from the DOCX file"
+        },
+        "extract_text_from_uploaded_image": {
+            "Functionality": "Uses Pytesseract to extract text from an uploaded image.",
+            "Parameters": "uploaded_image (image file), language (optional language for text extraction, default is 'eng')",
+            "Output": "Extracted text from the uploaded image"
+        },
+        # Add details for other methods similarly
+        "process_docx_text_without_lists": {
+            "Functionality": "Removes lists from DOCX text.",
+            "Parameters": "docx_file (DOCX file path)",
+            "Output": "Text without lists from the DOCX file"
+        },
+        "process_pdf_text_without_lists": {
+            "Functionality": "Extracts text from a PDF file without lists.",
+            "Parameters": "pdf_file (PDF file path)",
+            "Output": "Extracted text from the PDF file without lists"
+        },
+        "process_txt_file": {
+            "Functionality": "Reads and extracts text from a TXT file.",
+            "Parameters": "txt_file (TXT file object)",
+            "Output": "Text extracted from the TXT file"
+        },
+        "translate_text_with_google": {
+            "Functionality": "Translates text using Google Translate.",
+            "Parameters": "text (text to translate), target_language (language code for translation)",
+            "Output": "Translated text"
+        },
+        "convert_text_to_speech": {
+            "Functionality": "Converts text to speech (MP3 format).",
+            "Parameters": "text (text to convert), output_file (output file path), language (language code for speech synthesis)",
+            "Output": "MP3 audio file with the generated speech"
+        },
+        "get_binary_file_downloader_html": {
+            "Functionality": "Generates a download link for a file.",
+            "Parameters": "link_text (text for the download link), file_path (file path), file_format (file format)",
+            "Output": "HTML download link for the file"
+        },
+        "convert_text_to_word_doc": {
+            "Functionality": "Converts translated text to a Word document.",
+            "Parameters": "text (translated text to convert), output_file (output file path)",
+            "Output": "Word document containing the translated text"
+        }
+    }
 
-        # Button to clear null values
-        if st.button("Clear Null Values"):
-            modified_data = uploaded_data.dropna()  # Remove rows with null values
+    for method_name, details in method_info.items():
+        st.sidebar.subheader(method_name)
+        for key, value in details.items():
+            st.sidebar.text(f"{key}: {value}")
+        st.sidebar.text("\n")
 
-            # Display modified data
-            st.title("Modified Dataset:")
-            st.write(modified_data.head())
+def main():
+    st.image("jangirii.png", width=300)
+    st.title("Text Translation and Conversion to Speech ( MultiLingual )")
 
-            # Download the modified dataset as CSV
-            csv_file = modified_data.to_csv(index=False)
-            b64 = base64.b64encode(csv_file.encode()).decode()
-            href = f'<a href="data:file/csv;base64,{b64}" download="modified_dataset.csv">Download Modified Dataset</a>'
-            st.markdown(href, unsafe_allow_html=True)
-
-if __name__ == "__main__":
-    main()
-
+    if __name__ == "__main__":
+        display_method_info()
+        main()
