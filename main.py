3a6a33c23ad5639d185eef13bc3f7c70c664183d
Rahul6158 <128017087+Rahul6158@users.noreply.github.com>
2023-12-28 10:18:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import page3
 import datetime
 import pandas as pd
 from itertools import cycle
+import time
 
 # Read the CSV file containing the quotes and authors
 quotes_df = pd.read_csv('AnimeQuotes.csv')  # Replace 'AnimeQuotes.csv' with your file path
@@ -13,23 +14,23 @@ quotes_df = pd.read_csv('AnimeQuotes.csv')  # Replace 'AnimeQuotes.csv' with you
 quote_colors = cycle(["red", "blue", "green", "purple", "orange", "white", "brown", "black"])  # Add more colors as needed
 author_colors = cycle(["blue", "green", "purple", "red"])  # Corresponding colors for authors
 
-# Function to get the quote and author based on the current hour
-def get_hourly_quote():
+# Function to get the quote and author based on the current time
+def get_quote():
     current_time = datetime.datetime.now().strftime('%H:%M')
     quote_index = hash(current_time) % len(quotes_df)
     hourly_quote = quotes_df.iloc[quote_index]
     return hourly_quote['Quote'], hourly_quote['Character']  # Assuming 'Quote' and 'Character' are column names
-  # Assuming 'Quote' and 'Character' are column names
 
-# Function to display the hourly quote and author with rotating colors
+# Function to display the quote and author with rotating colors
 def display_quote():
-    quote, author = get_hourly_quote()
+    quote, author = get_quote()
     
     # Get next color in rotation for the quote and author
     quote_color = next(quote_colors)
     author_color = next(author_colors)
     
-    quote_html = f'<p style="color:{quote_color}; font-family: Lucida Console, Monaco, monospace; font-size: 20px;"><strong>{quote}</strong></p>'
+    quote_html = f'<h3 style="color:{quote_color}; font-family: Lucida Console, Monaco, monospace;">Timepass Quotes:</h3>'
+    quote_html += f'<p style="color:{quote_color}; font-family: Lucida Console, Monaco, monospace; font-size: 20px;"><strong>{quote}</strong></p>'
     author_html = f'<p style="color:{author_color}; font-family: Lucida Console, Monaco, monospace; font-size: 16px;"><em>- {author}</em></p>'
     
     full_html = quote_html + author_html
@@ -47,9 +48,12 @@ def custom_sidebar():
 page_choice = custom_sidebar()
 
 # Display the logo at the top of the main page
-logo = Image.open('logo.png')
-# Display the daily quote and author in the sidebar
-display_quote()
+st.image('logo.png', width=700)  # Replace 'logo.png' with your logo file and adjust the width as needed
+
+# Loop to update the quote every minute
+while True:
+    display_quote()
+    time.sleep(60)  # Update the quote every minute
 
 # Depending on the selected choice, call the respective main function
 if page_choice == "Document and Pdf Translation":
