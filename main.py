2d63e104838201de2dafdcd51389ef0951ac90db
Rahul6158 <128017087+Rahul6158@users.noreply.github.com>
2023-12-29 00:57:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ def custom_sidebar():
     st.sidebar.title("Features")
     st.sidebar.header("Available Options")  # Add a sidebar title
     # Create radio button group
-    page_choice = st.sidebar.radio("", ["Document and Pdf Translation", "Text Translation", "Text Summarization"])
+    page_choice = st.sidebar.radio("", ["Document and Pdf Translation", "Text Translation", "About the App"])
     
     return page_choice
 
@@ -72,5 +72,5 @@ if page_choice == "Document and Pdf Translation":
     page1.main()  # Call the main function for Page 1
 elif page_choice == "Text Translation":
     page2.main()
-elif page_choice == "Text Summarization":
-    page3.main()  # Call the main function for Page 3
+elif page_choice == "About the App":
+    page4.main()  # Call the main function for Page 4
